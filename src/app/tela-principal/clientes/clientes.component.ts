d6673efadfea7457242523f5b2f34198965a7c16
rebelattogustavo <gu.zapella@gmail.com>
2022-03-02 21:36:15 -0300

    update

--- src/app/tela-principal/clientes/clientes.component.ts
+++ src/app/tela-principal/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+  }
+
+  voltar(){
+    this.route.navigate(['/main/'])
   }
 
 }
--- src/app/tela-principal/produtos/produtos.component.ts
+++ src/app/tela-principal/produtos/produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+  }
+
+  voltar(){
+    this.route.navigate(['/main/'])
   }
 
 }
